8f46fef87902ab9582e9c70cf46bbbef85a48c66
jcipriano <>
2023-07-19 16:09:08 -0700

    adding next public env var

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -2,6 +2,7 @@ export default function handler(req, res) {
   res.status(200).json({
     NEXT_PUBLIC_NETLIFY: process.env.NEXT_PUBLIC_NETLIFY,
     DOTENV_FETCH_WITH: process.env.DOTENV_FETCH_WITH,
-    TOML_ENV_VAR: process.env.TOML_ENV_VAR
+    TOML_ENV_VAR: process.env.TOML_ENV_VAR,
+    NEXT_PUBLIC_TOML_ENV_VAR: process.env.NEXT_PUBLIC_TOML_ENV_VAR
   });
 }
