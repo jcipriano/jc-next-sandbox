0a84300dce510c08c6b127bf3dc391b4358c2251
jcipriano <>
2023-07-27 13:25:35 -0700

    test env var from .env

--- pages/index.js
+++ pages/index.js
@@ -4,10 +4,6 @@ import Footer from '@components/Footer'
 
 export default function Home() {
 
-
-  console.log(process.env.NEXT_PUBLIC_NETLIFY)
-
-
   return (
     <div className="container">
       <Head>
